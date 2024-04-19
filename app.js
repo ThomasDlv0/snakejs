73fb8109e7d70bd0432c3039e0d35bab40a8cab6
ThomasDlv0 <Thomas.delville000@icloud.com>
2024-04-19 16:21:06 +0200

    snake qui bouge plus pomme

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 
+// Vitesse sur X
+vx = 20;
 
-vx = 0;
-vy = -10;
+// Vitesse sur Y
+vy = -20;
+
+//PommeX
+let pommeX = 0;
+
+//PommeY
+let pommeY = 0;
+
 
 let snake = [
     {
@@ -11,15 +20,15 @@ let snake = [
         y: 150
     },
     {
-        x: 130,
-        y: 150
-    },
-    {
         x: 120,
         y: 150
     },
     {
-        x: 110,
+        x: 100,
+        y: 150
+    },
+    {
+        x: 80,
         y: 150
     }
 ]
@@ -30,6 +39,8 @@ function animation(){
 
         clearCanvas();
 
+        dessinApple()
+        
         dessinSnake();
 
         runSnake();
@@ -40,6 +51,7 @@ function animation(){
 }
 
 animation();
+createPomme();
 
 function clearCanvas() {
     ctx.fillStyle = "white";
@@ -52,8 +64,8 @@ function dessinSnakeMorceaux(morceau) {
     
     ctx.fillStyle = "#00fe14";
     ctx.strokeStyle = "black";
-    ctx.fillRect(morceau.x, morceau.y, 10, 10);
-    ctx.strokeRect(morceau.x, morceau.y, 10, 10);
+    ctx.fillRect(morceau.x, morceau.y, 20, 20);
+    ctx.strokeRect(morceau.x, morceau.y, 20, 20);
 }
 
 function dessinSnake() {
@@ -66,7 +78,69 @@ function dessinSnake() {
 function runSnake() {
     const head = {x: snake[0].x + vx, y: snake[0].y + vy};
     snake.unshift(head);
-    snake.pop();
+    
+    const  snakeEatApple = snake[0].x === pommeX && snake[0].y === pommeY;
+    
+    if (snakeEatApple) {
+        createPomme();
+    } else {
+        snake.pop();
+    }
+    
+}
+runSnake();
+
+document.addEventListener("keydown", changeDirection);
+
+function changeDirection(e) {
+    
+    const FLECHE_LEFT = 37;
+    const FLECHE_RIGHT = 39;
+    const FLECHE_TOP = 38;
+    const FLECHE_BOTTOM = 40;
+
+    const direction = e.keyCode;
+
+    const top = vy === -20;
+    const bottom = vy === 20;
+    const right = vx === 20;
+    const left = vx === -20;
+    
+    if (direction === FLECHE_LEFT && !right){vx = -20; vy = 0;}
+    if (direction === FLECHE_TOP && !bottom){vx = 0; vy = -20;}
+    if (direction === FLECHE_RIGHT && !left){vx = 20; vy = 0;}
+    if (direction === FLECHE_BOTTOM && !top){vx = 0; vy = 20;}
+    
 }
 
+function random(){
+    return Math.round((Math.random() * 690) / 20) * 20;
+}
+
+function createPomme() {
+    
+    pommeX = random();
+    pommeY = random();
+    
+    snake.forEach(function(part){
+        
+        const snakeOnApple = part.x == pommeX && part.y == pommeY;
+        
+        if (snakeOnApple) {
+            createPomme();
+        }
+        
+    })
+    
+    
+}
+
+function dessinApple() {
+    ctx.fillStyle = "red";
+    ctx.strokeStyle = "darkred";
+    ctx.beginPath();
+    ctx.arc(pommeX -10, pommeY, 5, 0, 4 * Math.PI);
+    ctx.fill();
+    ctx.stroke();
+}
 
